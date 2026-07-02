cfbf70bc85ea002a25a951a01e732267a18c3e5e
Danilorachadel-j <danilo_rachadel-j@>
2026-07-02 08:57:58 -0300

    add - exercicio 4 iniciado

--- ex_03.php
+++ ex_03.php
@@ -7,10 +7,8 @@ $cpf = preg_replace ('/[^0-9]/', '', $cpf);
 if (strlen($cpf) !== 11){
     return "CPF inválido";
 }
-return '***.***.***' . ($cpf, -4);
+return '***.***.***' . substr($cpf, -4);
 
 }
-
 echo mascararcpf("123.456.789.06");
 
-mascararcpf($cpf)
