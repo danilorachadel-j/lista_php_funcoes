5cc262211ea01c74aa3fef473e0849b3a1534e58
Danilorachadel-j <danilo_rachadel-j@>
2026-07-02 08:38:34 -0300

    add - exercicio 2 finalizado

--- ex_02.php
+++ ex_02.php
@@ -1,5 +1,19 @@
 <?php 
 
-function inverterTexto 
+function inverterTexto($text) {
 
-?>
+
+
+
+$invertido = strrev($text);
+$contarLetras = mb_strlen($text);
+
+
+echo "A palavras é : $text <br><br>";
+echo "A palavra ap contrário é : $invertido <br><br>";
+echo "A quantidade de caracteres é : $contarLetras <br><br>";
+}
+
+$text = "Vegeta";
+
+inverterTexto($text);
