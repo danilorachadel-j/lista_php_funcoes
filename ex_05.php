953b5f104d7b9fab8d4fe720b1810ef653d0179f
Danilorachadel-j <danilo_rachadel-j@>
2026-07-07 07:38:44 -0300

    add - adicionei o exercicio 05

---
+++ ex_05.php
@@ -0,0 +1,14 @@
+<?php 
+
+function analisarTexto($text) {
+
+$contarLetras = mb_strlen($text);
+$quantiVogais = 'a, e, i, o, u';
+$quantiConsoantes = 'b, c, d, f, g, h, j, k, l, m, n, p, q, r, s, t, v, w, x, y, z';
+
+
+echo " quantidade de palavras é: $contarPalavras"
+echo "A quantidade de caracteres é: $contarLetras <br><br>"
+}
+
+?>
