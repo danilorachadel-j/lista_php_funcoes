13b952c0dc6606206a8747810565cdd919a07942
Danilorachadel-j <danilo_rachadel-j>
2026-07-09 07:38:25 -0300

    add - adicionei o exercicio 07

--- ex_06.php
+++ ex_06.php
@@ -2,15 +2,28 @@
 
 function converterTemperatura($temp){
 
-$celsius = 'C';
-$fahrenheit = 'F';
-$kelvin = 'K';
+$tempNumero = 2;
+$celsius1 = 'C';
+$fahrenheit2 = 'F';
+$kelvin3 = 'K';
 
 
-if ($temp = $celsius){
+if ($temp = $celsius1){
+    if($temp02 = $fahrenheit2){
+$tempResultado = (($tempNumero * (9/5)) + 32);
+echo "$tempResultado";
 
+    } else{
+
+    $tempResultado = ($tempNumero);
+    echo "$tempResultado";
+    };
 }
 
 }
 
+$temp02 = 'K';
+$temp = 'C';
+
+converterTemperatura($temp);
 ?>
