5d13c9e79dab5c5000908927a40ea08f7c255d4b
Danilorachadel-j <danilo_rachadel-j@>
2026-07-09 08:15:57 -0300

    add - finalizei o exercicio 08

--- ex_08.php
+++ ex_08.php
@@ -1,9 +1,19 @@
-<?php 
+<?php
 
-function ordenarNomes(){
-
-
-
+function ordenarNomes($listaNomes) {
+    $vetorNomes = explode(',', $listaNomes);
+    
+    $vetorLimpo = array_map('trim', $vetorNomes);
+    
+    sort($vetorLimpo);
+    
+    return $vetorLimpo;
 }
+
+$alunos = " Mariana, Carlos, ana, Bruno,  julia ";
+
+$alunosOrdenados = ordenarNomes($alunos);
+
+print_r($alunosOrdenados);
 
 ?>
