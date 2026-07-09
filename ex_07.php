90d3bd0c485663a36b0dcea243714a5dce7d9550
Danilorachadel-j <danilo_rachadel-j@>
2026-07-09 07:54:49 -0300

    add - adicionei o exercicio 07 novamente e o finalizei

--- ex_07.php
+++ ex_07.php
@@ -1,1 +1,19 @@
+<?php 
 
+function calcularDesconto($valor){
+
+if ($valor >100 and <500) {
+    return $valor = $valor - ($valor*0.1);
+}
+if ($valor >500 and <1000) {
+    return $valor = $valor - ($valor*0.2);
+}
+if ($valor >1000) {
+    return $valor = $valor - ($valor*0.3);
+}else{
+    return $valor;
+}
+
+}
+
+?>
